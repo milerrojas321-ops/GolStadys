94635fc44ba89cf80bce4da86018afc9dae80423
Miller Rojas <milerrojas321@gmail.com>
2026-06-17 16:01:13 -0500

    1 hora

--- src/controllers/apuesta_controller.js
+++ src/controllers/apuesta_controller.js
@@ -43,7 +43,7 @@ export const registrarApuesta = async (req, res) => {
     const diferenciaHoras = (momentoPartido - momentoActual) / (1000 * 60 * 60);
 
     //  REGLA DE NEGOCIO: Bloqueo a menos de 5 horas del partido
-    if (diferenciaHoras < 5) {
+    if (diferenciaHoras < 0.5) {
       return res.status(400).json({
         ok: false,
         mensaje: 'Pronóstico bloqueado. No puedes fijar o editar marcadores a menos de 5 horas del partido.'
